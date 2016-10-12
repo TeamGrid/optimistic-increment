f6c793eb84eb4416199d8cfc3eca5e6a5656c291
Max Nowack <max@unsou.de>
2016-10-12 11:52:05 +0200

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'teamgrid:optimistic-increment',
-  version: '1.0.0',
+  version: '1.1.0',
   // Brief, one-line summary of the package.
   summary: 'Reactively increment values with a better user experience',
   // URL to the Git repository containing the source code for this package.
